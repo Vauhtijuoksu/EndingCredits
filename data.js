27ff09d8584baf31916940b6d3bc1929ac64de90
jiisloth <joonas.val@gmail.com>
2025-11-20 18:55:05 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,3 +1,10 @@
 const scrolltime = 6.5
 const endtext = 'Kiitos katsomisesta!'
-const credits = []
+const credits = [
+    {'type': 'bigheading', 'text': 'Vauhtijuoksu+ 2025 tekemässä:'},
+    {'type': 'heading', 'text': 'Vauhtijuoksu-toimikunta 2023'},
+    {'type': 'title', 'text': 'Päätuottaja'},
+    {'type': 'responsible', 'text': 'jsloth'},
+    {'type': 'title', 'text': 'Muut teki jottai'},
+    {'type': 'name', 'text': 'lisätään ne tähä myöhemmin^tm'},
+]
